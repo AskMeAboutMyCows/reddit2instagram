30f9e09288356f3724add53bb7c8da4190e814d1
AskMeAboutMyCows <fffarmer@protonmail.com>
2022-04-02 15:21:01 -0400

    unfinished but improved

--- main.py
+++ main.py
@@ -6,10 +6,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+#dotenv variables
 insta_pass = os.getenv("insta_pass")
 insta_username = os.getenv("insta_username")
 
-#put instagram login below
+#put instagram/reddit pass in .env file
 
 bot =Bot()
 bot.login(username="insta_username",password="insta_pass")
+
+#have to get this api from reddit dev
+reddit = praw.Reddit(client_id='',
+client_secret='',
+username='',
+password='',
+user_agent='')
+
+red='formuladank' #put the name of the subreddit that you want to pull from
+subreddit = reddit.subreddit(red)
+spicyMemes = subreddit.hot(limit=3)
